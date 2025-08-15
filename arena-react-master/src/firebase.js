2b08f587a8fb295a2c9c290d6b224741638aac37
Unicorn2700 <singhritikdtu@gmail.com>
2025-08-15 22:26:58 +0530

    Removed Firebase API key

--- arena-react-master/src/firebase.js
+++ arena-react-master/src/firebase.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: "",
     authDomain: "arena-ducs.firebaseapp.com",
     databaseURL: "https://arena-ducs-default-rtdb.firebaseio.com",
     projectId: "arena-ducs",
